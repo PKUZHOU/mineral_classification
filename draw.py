91ae896dcd604f16b6ca1931d6d9396f74ef1210
zhouzhe <zhou.zhe@pku.edu.cn>
2019-04-01 15:48:22 +0800

    add more files

---
+++ draw.py
@@ -0,0 +1,49 @@
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+# plt.rcParams['font.sans-serif']=['SimHei']
+# plt.rcParams['axes.unicode_minus']=False
+def draw_loss():
+    pdf = PdfPages("loss.pdf")
+
+    with open('loss_log.txt','r') as f:
+        figure = plt.figure()
+        loss = f.readlines()[0].strip().split(" ")
+        points = []
+        for point in loss:
+            # point = point[7:13]
+            points.append(float(point))
+        x = range(len(points))
+        plt.plot(x,points[:len(x)])
+        plt.xlabel("batch",fontsize=12)
+        plt.ylabel("loss",fontsize=12)
+        plt.tight_layout()
+        plt.show()
+        pdf.savefig(figure)
+        plt.close()
+        pdf.close()
+
+def draw_train_acc():
+    pdf = PdfPages("train_acc.pdf")
+
+    with open('train_acc.txt', 'r') as f:
+        figure = plt.figure()
+        loss = f.readlines()[0].strip().split(" ")
+        points = []
+        for point in loss:
+            # point = point[7:13]
+            points.append(float(point))
+        x = range(len(points))
+        plt.plot(x, points[:len(x)])
+        plt.axis([0,4,0.98,1.0])
+        # plt.set_ylim(0.95,1.0)
+        plt.xlabel("epoch", fontsize=12)
+        plt.ylabel("accuracy", fontsize=12)
+        plt.tight_layout()
+        plt.show()
+        pdf.savefig(figure)
+        plt.close()
+        pdf.close()
+
+if __name__ == '__main__':
+    draw_train_acc()
